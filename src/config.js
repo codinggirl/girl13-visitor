04de69abef08bba786ff99f42c0c5172751bb2f8
Richard Libre <meng@codinggirl.net>
2019-06-04 00:35:00 +0800

    recoding: from sqlite3 to file

--- src/config.js
+++
@@ -1,3 +0,0 @@
-
-export let startIndex = 4
-export let endIndex = 28799
--- src/index.js
+++ src/index.js
@@ -1,37 +1,66 @@
 
-import {startIndex, endIndex} from './config.js'
+
 import fetchContent from './fetchContent.js'
-import saveData from './saveData.js';
-import prepareTable from './prepareTable.js'
+import fs from 'fs'
+import path from 'path'
+import got from 'got'
+import dotenv from 'dotenv'
+import _debug from 'debug'
+import moment from 'moment-timezone'
+
+dotenv.config()
+const debug = _debug('...')
 
 async function main() {
 
-    await prepareTable()
+    console.log('task started.')
 
-    let ps = []
-    let maxRunning = 100
-    // url
-    let j = 0
+    console.log('prepare urls.')
+    let pendingPages = []
+    const startIndex = process.env.SITE_PAGE_MIN_ID
+    const endIndex = process.env.SITE_PAGE_MAX_ID
     for (let i = startIndex; i <= endIndex; i++) {
-        if (j === maxRunning) {
-            j = 0
-
-            let result = await Promise.all(ps)
-            let saveImagePs = []
-            result.map(item => {
-                saveImagePs.push(saveData(item))
-            })
-            await Promise.all(saveImagePs)
-
-            // save images
-            ps = []
-        }
         let url = `http://www.girl13.com/${i}.html`
-        let p = fetchContent(url)
-        ps.push(p)
-
-        j++
+        pendingPages.push({
+            index: i,
+            url: url
+        })
     }
+    console.log('prepare urls done.')
+
+    console.log('fetch contents.')
+    while (pendingPages.length > 0) {
+        const size = process.env.MAX_TASK_COUNT
+        const currentItems = pendingPages.splice(0, size)
+        const ps = currentItems.map(async page => {
+            // meta
+            let metaFilePath = path.resolve(process.env.META_DATA_DIR, `${page.index}.meta.json`)
+            if (!fs.existsSync(metaFilePath)) {
+                let p = await fetchContent(page.url)
+                if (p) {
+                    p.index = page.index
+                    p.created_at = moment.tz('Asia/Shanghai').format()
+                    
+                    console.log(`write data to: ${metaFilePath}`)
+                    fs.writeFileSync(metaFilePath, JSON.stringify(p, null, 4))
+                }
+            }
+            // img
+            if (fs.existsSync(metaFilePath)) {
+                let imageFilePath = path.resolve(process.env.IMAGE_DATA_DIR, `${page.index}.image.jpg`)
+                if (!fs.existsSync(imageFilePath)) {
+                    try {
+                        let meta = JSON.parse(fs.readFileSync(metaFilePath))
+                        got.stream(meta.img).pipe(fs.createWriteStream(imageFilePath))
+                    } catch (e) {
+                        debug(e)
+                    }
+                }
+            }         
+        })
+        await Promise.all(ps)
+    }
+    console.log('fetch contents done.')
 }
 
 (async () => {
